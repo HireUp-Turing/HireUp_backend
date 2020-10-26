e03bfde2ac582d1c786da68566cd565aa1389d34
Gaby Mendez <gmendez90@gmail.com>
2020-10-25 22:35:22 -0400

    Set up CORS and flask-restful in #create_app

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,4 @@
-# from flask_cors import CORS
+from flask_cors import CORS
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, jsonify
@@ -17,6 +17,13 @@ def create_app(config_name='default'):
 
     # set up database
     db.init_app(app)
+
+    # set up CORS
+    CORS(app, resources={r"/*": {"origins": "*"}})
+
+    # set up Flask-RESTful
+    # this allows methods like api.add_resource to be used later on
+    api = Api(app)
 
     # test route
     @app.route('/')
