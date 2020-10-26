693184ca46aee1e290f1681ac9dbb47cf0354f0f
Gaby Mendez <gmendez90@gmail.com>
2020-10-25 21:20:10 -0400

    Add manage.py file and create a 'routes' command to test it

--- manage.py
+++ manage.py
@@ -1,1 +1,20 @@
-# File to configure Manager from flask-script, which is like Rake tasks for Flask  
+# File to configure Manager from flask-script, which is like Rake tasks for Flask
+from flask_script import Manager
+# include the next line when dealing with migrations
+# from flask_migrate import Migrate, MigrateCommand
+
+from api import create_app, db
+# doesn't exist yet
+# from api.database.models import User
+
+app = create_app()
+# migrate = Migrate(app, db)
+manager = Manager(app)
+
+@manager.command
+# equivalent to 'rake routes' in Rails
+def routes():
+    print(app.url_map)
+
+if __name__ == "__main__":
+    manager.run()
