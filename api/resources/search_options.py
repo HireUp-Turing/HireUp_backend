07ad285d7a5f04d642718d5686e2aa87cfbbe6dd
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-10-28 15:47:48 -0600

    Build out response containing all skills and values
    Co-authored-by: Gaby Mendez 
    <62727545+gabichuelas@users.noreply.github.com>

--- api/resources/search_options.py
+++ api/resources/search_options.py
@@ -8,9 +8,26 @@ from sqlalchemy.orm.exc import NoResultFound
 from api import db
 from api.database.models import Applicant, Skill, Value
 
-def _options_payload(skills, values):
+def _options_payload(options):
 
     return {
-        'skills': [{'id': skill.id, 'attribute': skill.name} for skill in skills],
-        'values': [{'id': value.id, 'attribute': value.name} for value in values]
+        'skills': [{'id': skill.id, 'attribute': skill.name} for skill in options['skills']],
+        'values': [{'id': value.id, 'attribute': value.name} for value in options['values']]
     }
+
+class SearchOptionsResource(Resource):
+    """
+    this Resource file is for our /search-options endpoints
+    """
+
+    def get(self, *args, **kwargs):
+        skills = Skill.query.all()
+        values = Value.query.all()
+        options = {"skills": skills, "values": values}
+
+        results = [_options_payload(options)]
+
+        return {
+            'success': True,
+            'data': results
+        }
