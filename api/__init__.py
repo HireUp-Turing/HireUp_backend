0e93de583237c9d2c9168e63374d43e516560e31
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-10-29 11:38:55 -0600

    Add /api/v1/search route

--- api/__init__.py
+++ api/__init__.py
@@ -34,11 +34,13 @@ def create_app(config_name='default'):
     from api.resources.applicants import ApplicantsResource, ApplicantResource
     from api.resources.messages import MessagesResource, MessageResource
     from api.resources.search_options import SearchOptionsResource
+    from api.resources.search_results import SearchResultsResource
 
     api.add_resource(ApplicantResource, '/api/v1/applicants/<applicant_id>')
     api.add_resource(ApplicantsResource, '/api/v1/applicants')
     api.add_resource(MessageResource, '/api/v1/messages/<message_id>')
     api.add_resource(MessagesResource, '/api/v1/messages')
     api.add_resource(SearchOptionsResource, '/api/v1/search-options')
+    api.add_resource(SearchResultsResource, '/api/v1/search')
 
     return app
