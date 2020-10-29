36273f6bc9d0fcdd944a88e7008ba1316a08724a
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-10-29 03:43:46 -0600

    SearchOptions only returns skills and values actively associated with profiles

--- api/resources/search_options.py
+++ api/resources/search_options.py
@@ -21,8 +21,10 @@ class SearchOptionsResource(Resource):
     """
 
     def get(self, *args, **kwargs):
-        skills = Skill.query.all()
-        values = Value.query.all()
+        skills = db.engine.execute("SELECT DISTINCT skills.id, skills.name FROM applicant_skills JOIN applicants ON applicants.id = applicant_skills.applicant_id JOIN skills ON applicant_skills.skill_id = skills.id ORDER BY skills.name")
+
+        values = db.engine.execute("SELECT DISTINCT values.id, values.name FROM applicant_values JOIN applicants ON applicants.id = applicant_values.applicant_id JOIN values ON applicant_values.value_id = values.id ORDER BY values.name")
+
         options = {"skills": skills, "values": values}
 
         results = [_options_payload(options)]
