a76c960e76834fce7df1275a4274d2d3ae2a0993
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-10-29 14:59:31 -0600

    Define search_results_paylod

--- api/resources/search_results.py
+++ api/resources/search_results.py
@@ -7,3 +7,14 @@ from sqlalchemy.orm.exc import NoResultFound
 
 from api import db
 from api.database.models import Applicant
+
+def _search_results_payload(results):
+
+    return {
+        'id': applicant.id,
+        'username': applicant.username,
+        'email': applicant.email,
+        'bio': applicant.bio,
+        'skills': [skill.name for skill in applicant.skills],
+        'values': [value.name for value in applicant.values],
+    }
