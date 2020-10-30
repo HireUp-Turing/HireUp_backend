075ef27dcefdf6e91f1a6fdc5dfcb745fabae5d8
Gaby Mendez <gmendez90@gmail.com>
2020-10-29 20:29:08 -0400

    Change import models line to import Skill

--- api/resources/skills.py
+++ api/resources/skills.py
@@ -6,7 +6,7 @@ from flask_restful import Resource, abort
 from sqlalchemy.orm.exc import NoResultFound
 
 from api import db
-from api.database.models import Applicant, ApplicantSkill, ApplicantValue
+from api.database.models import Skill
 
 # HELPER METHODS
 def _skill_payload(skill):
@@ -24,7 +24,7 @@ class Skills(Resource):
     def get(self, *args, **kwargs):
         skills = Skill.query.all()
         results = [_skill_payload(skill) for skill in skills]
-        
+
         return {
             'success': True,
             'data': results
