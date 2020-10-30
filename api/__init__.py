f7b9c043733ccec70e769b83d2a11058781ff92b
Gaby Mendez <gmendez90@gmail.com>
2020-10-29 20:25:02 -0400

    Add routes for /skills and /values in #create_app

--- api/__init__.py
+++ api/__init__.py
@@ -34,11 +34,15 @@ def create_app(config_name='default'):
     from api.resources.applicants import ApplicantsResource, ApplicantResource
     from api.resources.messages import MessagesResource, MessageResource
     from api.resources.search_options import SearchOptionsResource
+    from api.resources.skills import SkillsResource
+    from api.resources.values import ValuesResource
 
     api.add_resource(ApplicantResource, '/api/v1/applicants/<applicant_id>')
     api.add_resource(ApplicantsResource, '/api/v1/applicants')
     api.add_resource(MessageResource, '/api/v1/messages/<message_id>')
     api.add_resource(MessagesResource, '/api/v1/messages')
     api.add_resource(SearchOptionsResource, '/api/v1/applicants/search-options')
+    api.add_resource(SkillsResource, '/api/v1/skills')
+    api.add_resource(ValuesResource, '/api/v1/values')
 
     return app
