c4f36c08528cdc489dece37f69aece2645b302d2
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-11-01 19:29:35 -0700

    Use SQLAlchemy to produce successful response

--- api/resources/search_results.py
+++ api/resources/search_results.py
@@ -6,11 +6,11 @@ from flask_restful import Resource, abort
 from sqlalchemy.orm.exc import NoResultFound
 
 from api import db
-from api.database.models import Applicant, ApplicantSkill, ApplicantValue
+from api.database.models import Applicant, Skill, Value
 
 # HELPER METHODS
 def _applicant_payload(applicant):
-    import pdb; pdb.set_trace()
+
     return {
         'id': applicant.id,
         'username': applicant.username,
@@ -20,33 +20,33 @@ def _applicant_payload(applicant):
         'values': [value.name for value in applicant.values],
     }
 
-def _create_sql_query(skill_ids, value_ids):
-    sql_query = "SELECT DISTINCT applicants.* FROM applicants JOIN applicant_skills ON applicants.id = applicant_skills.applicant_id JOIN applicant_values ON applicants.id = applicant_values.applicant_id WHERE "
-    skill_ids_length = len(skill_ids) - 1
-    value_ids_length = len(value_ids) - 1
-    if skill_ids and value_ids:
-        for skill_id in skill_ids:
-            sql_query = sql_query + f"applicant_skills.skill_id = {skill_id} OR "
-        for value_id in value_ids:
-            if value_id != value_ids[value_ids_length]:
-                sql_query = sql_query + f"applicant_values.value_id = {value_id} OR "
-            else:
-                sql_query = sql_query + f"applicant_values.value_id = {value_id}"
-        return sql_query
-    elif skill_ids and not value_ids:
-        for skill_id in skill_ids:
-            if skill_id != skill_ids[skill_ids_length]:
-                sql_query = sql_query + f"applicant_skills.skill_id = {skill_id} OR "
-            else:
-                sql_query = sql_query + f"applicant_skills.skill_id = {skill_id}"
-        return sql_query
-    elif value_ids and not skill_ids:
-        for value_id in value_ids:
-            if value_id != value_ids[value_ids_length]:
-                sql_query = sql_query + f"applicant_values.value_id = {value_id} OR "
-            else:
-                sql_query = sql_query + f"applicant_values.value_id = {value_id}"
-        return sql_query
+# def _create_sql_query(skill_ids, value_ids):
+#     sql_query = "SELECT DISTINCT applicants.* FROM applicants JOIN applicant_skills ON applicants.id = applicant_skills.applicant_id JOIN applicant_values ON applicants.id = applicant_values.applicant_id WHERE "
+#     skill_ids_length = len(skill_ids) - 1
+#     value_ids_length = len(value_ids) - 1
+#     if skill_ids and value_ids:
+#         for skill_id in skill_ids:
+#             sql_query = sql_query + f"applicant_skills.skill_id = {skill_id} OR "
+#         for value_id in value_ids:
+#             if value_id != value_ids[value_ids_length]:
+#                 sql_query = sql_query + f"applicant_values.value_id = {value_id} OR "
+#             else:
+#                 sql_query = sql_query + f"applicant_values.value_id = {value_id}"
+#         return sql_query
+#     elif skill_ids and not value_ids:
+#         for skill_id in skill_ids:
+#             if skill_id != skill_ids[skill_ids_length]:
+#                 sql_query = sql_query + f"applicant_skills.skill_id = {skill_id} OR "
+#             else:
+#                 sql_query = sql_query + f"applicant_skills.skill_id = {skill_id}"
+#         return sql_query
+#     elif value_ids and not skill_ids:
+#         for value_id in value_ids:
+#             if value_id != value_ids[value_ids_length]:
+#                 sql_query = sql_query + f"applicant_values.value_id = {value_id} OR "
+#             else:
+#                 sql_query = sql_query + f"applicant_values.value_id = {value_id}"
+#         return sql_query
 
 class SearchResultsResource(Resource):
     """
@@ -56,7 +56,9 @@ class SearchResultsResource(Resource):
     def get(self, **kwargs):
         skill_ids = request.json['skills']
         value_ids = request.json['values']
-        sql_query = _create_sql_query(skill_ids, value_ids)
+
+        # sql_query = _create_sql_query(skill_ids, value_ids)
+        # import pdb; pdb.set_trace()
         if not skill_ids and not value_ids:
             return {
                 'success': False,
@@ -71,8 +73,11 @@ class SearchResultsResource(Resource):
         #         'errors': "skill_ids and value_ids keys must be present in request body."
         #     }, 400
         else:
-            filtered_applicants = db.engine.execute(sql_query)
-            search_results = [_applicant_payload(applicant) for applicant in filtered_applicants]
+            # filtered_applicants = db.engine.execute(sql_query)
+            # search_results = [_applicant_payload(applicant) for applicant in filtered_applicants]
+
+            applicants = db.session.query(Applicant).join(Skill, Applicant.skills).filter(Skill.id == 1)
+            search_results = [_applicant_payload(applicant) for applicant in applicants]
             return {
                 'success': True,
                 'data': search_results
