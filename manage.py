59c0e4e9ff7aaa79850d3f7e58c552d153366291
Gaby Mendez <gmendez90@gmail.com>
2020-11-03 11:13:40 -0500

    Edit readme testing section and setup section

--- manage.py
+++ manage.py
@@ -318,27 +318,27 @@ def db_seed():
     # counts objects seeded
     print(f'obj count: {len(db.session.query(Applicant).all())}')
 
-@manager.command
-def test():
-    """run tests without coverage"""
-    tests = unittest.TestLoader().discover('.')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-
-@manager.command
-def cov():
-    """run unit tests with coverage"""
-    cov = coverage.coverage(branch=True, include='projects/*')
-    cov.start()
-    tests = unittest.TestLoader().discover('.')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-    cov.stop()
-    cov.save()
-    print('Coverage Summary:')
-    cov.report()
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    covdir = os.path.join(basedir, 'coverage')
-    cov.html_report(directory=covdir)
-    cov.erase()
+# @manager.command
+# def test():
+#     """run tests without coverage"""
+#     tests = unittest.TestLoader().discover('.')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
+#
+# @manager.command
+# def cov():
+#     """run unit tests with coverage"""
+#     cov = coverage.coverage(branch=True, include='projects/*')
+#     cov.start()
+#     tests = unittest.TestLoader().discover('.')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
+#     cov.stop()
+#     cov.save()
+#     print('Coverage Summary:')
+#     cov.report()
+#     basedir = os.path.abspath(os.path.dirname(__file__))
+#     covdir = os.path.join(basedir, 'coverage')
+#     cov.html_report(directory=covdir)
+#     cov.erase()
 
 
 if __name__ == "__main__":
